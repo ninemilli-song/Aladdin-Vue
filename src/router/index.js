6cdadb9668cc1a020a2ba9d1b7aae6c3bb94b2c8
ninemilli_song <ninemilli_song@163.com>
2018-09-13 14:22:41 +0800

    Play tslint

--- src/router/index.js
+++ src/router/index.js
@@ -1,35 +1,35 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import LyTab from 'ly-tab';
-import First from '../pages/quarter/First';
-import Second from '@/pages/quarter/Second';
-import Third from '@/pages/quarter/Third';
-import Fourth from '@/pages/quarter/Fourth';
+import First from '../pages/quarter/First.vue';
+import Second from '../pages/quarter/Second.vue';
+import Third from '../pages/quarter/Third.vue';
+import Fourth from '../pages/quarter/Fourth.vue';
 
 Vue.use(Router);
 Vue.use(LyTab);
 
 export default new Router({
-	routes: [
-		{
-			path: '/year/',
-			name: 'First',
-			component: First
-		},
-		{
-			path: '/year/second',
-			name: 'Second',
-			component: Second
-		},
-		{
-			path: '/year/third', 
-			name: 'Third',
-			component: Third
-		},
-		{
-			path: '/year/fourth',
-			name: 'Fourth',
-			component: Fourth
-		}
-	]
+    routes: [
+        {
+            path: '/year/',
+            name: 'First',
+            component: First
+        },
+        {
+            path: '/year/second',
+            name: 'Second',
+            component: Second
+        },
+        {
+            path: '/year/third',
+            name: 'Third',
+            component: Third
+        },
+        {
+            path: '/year/fourth',
+            name: 'Fourth',
+            component: Fourth
+        }
+    ]
 });
